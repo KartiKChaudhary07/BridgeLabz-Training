2cc1626bb01c517853db93fdf444682c9ea3905d
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-17 15:54:03 +0530

    volume of cylinder

---
+++ gcr-codebase/programming-elements/volumeofcylinder.java
@@ -0,0 +1,10 @@
+class VolumeOfCylinder {
+    public static void main(String[] args) {
+        double radius = 7;
+        double height = 10;
+
+        double volume = Math.PI * radius * radius * height;
+
+        System.out.println("Volume of Cylinder = " + volume);
+    }
+}
