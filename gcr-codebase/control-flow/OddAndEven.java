08ec71cf854e5ba9a383af73e2772bde8ae1814d
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-19 16:18:16 +0530

    [kartik] Add. Find Odd And Even

---
+++ gcr-codebase/control-flow/OddAndEven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class OddAndEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            for (int i = 1; i <= n; i++)
+                System.out.println(i + " is " + (i % 2 == 0 ? "Even" : "Odd"));
+            sc.close();
+        }
+    }
+}
