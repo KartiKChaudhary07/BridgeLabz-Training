5ede877eedd3bd6e996e426e1096a9555aa522c0
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-19 15:56:55 +0530

    [kartik] Add. Sum of N Natural Numbers

--- gcr-codebase/control-flow/NumberCheck.java
+++ gcr-codebase/control-flow/NumberCheck.java
@@ -7,5 +7,6 @@ class NumberCheck {
         if (num > 0) System.out.println("Positive");
         else if (num < 0) System.out.println("Negative");
         else System.out.println("Zero");
+        sc.close();
     }
 }
---
+++ gcr-codebase/control-flow/SumOfNaturalNumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int sum = 0, i = 1;
+            while (i <= n) sum += i++;
+
+            int formula = n * (n + 1) / 2;
+            System.out.println(sum + " " + formula);
+            sc.close();
+        }
+    }
+}
