caacccd0d48acac43f5206cdaad359a95d30d014
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-19 16:48:32 +0530

    [kartik] Update. Greatest factors using Greatest Factor using for loop

--- gcr-codebase/control-flow/GreatestfactorUsingFor.java
+++ gcr-codebase/control-flow/GreatestfactorUsingFor.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class GreatestfactorUsingWhile {
+class GreatestfactorUsingFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), gf = 1;
