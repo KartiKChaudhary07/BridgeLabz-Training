930e767973eb35155aed4440393a3e9fe4fb4db5
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-19 15:34:09 +0530

    Largest Among Three Solution

--- gcr-codebase/control-flow/LargestAmongThree.java
+++ gcr-codebase/control-flow/LargestAmongThree.java
@@ -1,8 +1,11 @@
 import java.util.*;
-class SmallestOfThree {
+class LargestCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
-        System.out.println((a < b && a < c));
+
+        System.out.println( (a > b && a > c));
+        System.out.println( (b > a && b > c));
+        System.out.println( (c > a && c > b));
     }
 }
