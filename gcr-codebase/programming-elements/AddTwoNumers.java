28735cee699d37737d1516248dfc80f77f3a1726
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-19 10:52:22 +0530

    ADD TWO NUMBERS

---
+++ gcr-codebase/programming-elements/AddTwoNumers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class AddTwoNumers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1, num2, sum;
+
+        System.out.print("Enter first number: ");
+        num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        num2 = sc.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
