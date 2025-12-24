c0f983b428066229295caabf04b71aeefdb0bd12
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-24 14:59:38 +0530

    [Kartik] Add. Chocolates Distribution

---
+++ gcr-codebase/methods/Chocolates.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Chocolates {
+
+    static int[] distribute(int chocolates, int children) {
+        return new int[]{chocolates / children, chocolates % children};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+
+        int[] res = distribute(chocolates, children);
+
+        System.out.println("Each child gets = " + res[0]);
+        System.out.println("Remaining chocolates = " + res[1]);
+
+        sc.close();
+    }
+}
