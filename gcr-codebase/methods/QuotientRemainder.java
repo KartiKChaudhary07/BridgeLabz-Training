bcd269e15a9b3677e350840ba3f9eff5a25319ac
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-24 14:58:21 +0530

    [Kartik] Add.  Quotient and Remainder

---
+++ gcr-codebase/methods/QuotientRemainder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class QuotientRemainder {
+
+    static int[] findRemainderAndQuotient(int number, int divisor) {
+        return new int[]{number / divisor, number % divisor};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int divisor = sc.nextInt();
+
+        int[] res = findRemainderAndQuotient(number, divisor);
+
+        System.out.println("Quotient = " + res[0]);
+        System.out.println("Remainder = " + res[1]);
+
+        sc.close();
+    }
+}
