1ff00ea5908d58aac479b5e3871f8f1d41341aee
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-30 21:37:22 +0530

    [Kartik] Add PalindromeChecker class for string validation

---
+++ gcr-codebase/PalindromeChecker.java
@@ -0,0 +1,19 @@
+class PalindromeChecker {
+    String text;
+
+    boolean isPalindrome() {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        String rev = new StringBuilder(clean).reverse().toString();
+        return clean.equals(rev);
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker p = new PalindromeChecker();
+        p.text = "A man a plan a canal Panama";
+
+        if (p.isPalindrome())
+            System.out.println(p.text + " is palindrome");
+        else
+            System.out.println(p.text + " is not Palindrome");
+    }
+}
