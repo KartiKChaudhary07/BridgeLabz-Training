12753ec8066ff9e25c5c76964bb668ab9fea0a2b
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-30 21:34:01 +0530

    [Kartik] Add BankAccount class with deposit & withdraw logic

---
+++ gcr-codebase/BankAccount.java
@@ -0,0 +1,31 @@
+class BankAccount {
+    double balance = 700;
+
+    void deposit(double amt) {
+        balance += amt;
+        System.out.println("Deposited: " + amt);
+    }
+
+    void withdraw(double amt) {
+        if (amt <= balance) {
+            balance -= amt;
+            System.out.println("Withdraw amount is : " + amt);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    void showBalance() {
+        System.out.println("Current balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        acc.showBalance();
+        acc.deposit(200);
+        acc.showBalance();
+        acc.withdraw(100);
+        acc.showBalance();
+        acc.withdraw(1000);
+    }
+}
