41710c8e567b1eb20d6890f64923dd0afdc81f96
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-30 21:37:59 +0530

    [Kartik] Add CartItem class for shopping cart operations

---
+++ gcr-codebase/CartItem.java
@@ -0,0 +1,30 @@
+class CartItem {
+    String itemName;
+    double price;
+    int quantity;
+
+    void add(int q) {
+        quantity += q;
+        System.out.println("Added " + q + " of " + itemName);
+    }
+
+    void remove(int q) {
+        quantity -= q;
+        System.out.println("Removed " + q + " of " + itemName);
+    }
+
+    void totalCost() {
+        System.out.println("Total cost: $" + (price * quantity));
+    }
+
+    public static void main(String[] args) {
+        CartItem c = new CartItem();
+        c.itemName = "Laptop";
+        c.price = 999.99;
+        c.quantity = 1;
+
+        c.add(2);
+        c.remove(1);
+        c.totalCost();
+    }
+}
