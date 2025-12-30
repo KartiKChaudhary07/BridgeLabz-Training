abf4aecc65c01460831263ec1ca0e20ff0b7625c
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2025-12-30 21:37:37 +0530

    [Kartik] Add MovieTicket class for ticket booking system

---
+++ gcr-codebase/MovieTicket.java
@@ -0,0 +1,21 @@
+class MovieTicket {
+    boolean booked = false;
+
+    void book(String movie, String seat, double price) {
+        if (!booked) {
+            booked = true;
+            System.out.println("Ticket booked for movie: " + movie);
+            System.out.println("Seat Number: " + seat);
+            System.out.println("Price: $" + price);
+        } else {
+            System.out.println("House full!!! sorry..... Ticket already booked");
+        }
+    }
+
+    public static void main(String[] args) {
+        MovieTicket t = new MovieTicket();
+        System.out.println("Ticket have not booked yet....");
+        t.book("Dragon", "A10", 120);
+        t.book("Dragon", "A10", 120);
+    }
+}
