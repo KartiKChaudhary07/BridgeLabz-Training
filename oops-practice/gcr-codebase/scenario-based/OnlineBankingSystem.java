3ea864b0125ac26cfa2179b35cac9089de93e03e
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2026-01-12 12:45:00 +0530

    [Kartik] Update. Add Online Banking System with OOP, inheritance, interface, and exception handling

--- oops-practice/gcr-codebase/scenario-based/OnlineBankingSystem.java
+++ oops-practice/gcr-codebase/scenario-based/OnlineBankingSystem.java
@@ -1,3 +1,11 @@
+import java.util.*;
+
+interface BankService {
+    void deposit(double amount);
+    void withdraw(double amount) throws InsufficientBalanceException;
+    double getBalance();
+    double calculateInterest();
+}
 
 class InsufficientBalanceException extends Exception {
     public InsufficientBalanceException(String msg) {
@@ -5,88 +13,83 @@ class InsufficientBalanceException extends Exception {
     }
 }
 
-class Account {
-    protected int accountNumber;
+abstract class Account implements BankService {
+    protected int accountNo;
+    protected String name;
     protected double balance;
+    protected List<String> transactions = new ArrayList<>();
 
-    Account(int accountNumber, double balance) {
-        this.accountNumber = accountNumber;
+    public Account(int accountNo, String name, double balance) {
+        this.accountNo = accountNo;
+        this.name = name;
         this.balance = balance;
     }
 
-    synchronized void withdraw(double amount)
-            throws InsufficientBalanceException {
-        if (balance < amount) {
-            throw new InsufficientBalanceException("Insufficient Balance");
-        }
-        balance -= amount;
-    }
-
-    synchronized void deposit(double amount) {
+    public void deposit(double amount) {
         balance += amount;
+        transactions.add("Deposited: " + amount);
     }
 
-    double getBalance() {
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > balance)
+            throw new InsufficientBalanceException("Insufficient Balance!");
+        balance -= amount;
+        transactions.add("Withdrawn: " + amount);
+    }
+
+    public double getBalance() {
         return balance;
+    }
+
+    public void showTransactions() {
+        System.out.println("Transaction History:");
+        for (String t : transactions)
+            System.out.println(t);
     }
 }
 
 class SavingsAccount extends Account {
-    SavingsAccount(int accNo, double balance) {
-        super(accNo, balance);
+    public SavingsAccount(int accNo, String name, double bal) {
+        super(accNo, name, bal);
     }
 
-    double calculateInterest() {
+    public double calculateInterest() {
         return balance * 0.04;
     }
 }
 
 class CurrentAccount extends Account {
-    CurrentAccount(int accNo, double balance) {
-        super(accNo, balance);
+    public CurrentAccount(int accNo, String name, double bal) {
+        super(accNo, name, bal);
     }
-}
 
-interface BankService {
-    void transfer(Account from, Account to, double amount)
-            throws InsufficientBalanceException;
-}
-
-class BankServiceImpl implements BankService {
-    public synchronized void transfer(Account from, Account to, double amount)
-            throws InsufficientBalanceException {
-        from.withdraw(amount);
-        to.deposit(amount);
+    public double calculateInterest() {
+        return balance * 0.02;
     }
 }
 
 public class OnlineBankingSystem {
     public static void main(String[] args) {
+        try {
+            Account a1 = new SavingsAccount(101, "Kartik", 10000);
+            Account a2 = new CurrentAccount(102, "Rahul", 5000);
 
-        SavingsAccount acc1 = new SavingsAccount(1001, 10000);
-        CurrentAccount acc2 = new CurrentAccount(2001, 5000);
+            a1.deposit(2000);
+            a1.withdraw(1500);
 
-        BankService service = new BankServiceImpl();
+            a2.deposit(3000);
+            a2.withdraw(1000);
 
-        Thread t1 = new Thread(() -> {
-            try {
-                service.transfer(acc1, acc2, 3000);
-                System.out.println("Transfer Successful");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        });
+            System.out.println("Savings Balance: " + a1.getBalance());
+            System.out.println("Savings Interest: " + a1.calculateInterest());
+            a1.showTransactions();
 
-        Thread t2 = new Thread(() -> {
-            try {
-                service.transfer(acc1, acc2, 4000);
-                System.out.println("Transfer Successful");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        });
+            System.out.println("\nCurrent Balance: " + a2.getBalance());
+            System.out.println("Current Interest: " + a2.calculateInterest());
+            a2.showTransactions();
 
-        t1.start();
-        t2.start();
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
